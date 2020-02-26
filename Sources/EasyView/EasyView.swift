d7a7904b4d6ce9aa72f51a8cd9658ff2da1e1786
Richard Wagstaff <rjwagstaff5@gmail.com>
2020-02-26 20:14:20 +0000

    first commit

--- Sources/EasyView/EasyView.swift
+++ Sources/EasyView/EasyView.swift
@@ -1,3 +1,50 @@
-struct EasyView {
-    var text = "Hello, World!"
+import UIKit
+
+class EasyView: UIView {
+    
+    var addViews: [UIView] = []
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        sharedInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        addSubviews()
+        configureUI()
+    }
+    
+    /**
+     Override this to add subviews manually. Otherwise override addViews variable to add them automatically
+     */
+    func addSubviews() {
+        for view in addViews {
+            addSubview(view)
+        }
+    }
+    
+    /**
+     Override this and setup the ui
+     */
+    func configureUI() {
+        
+    }
+
+    ///
+    func align() {
+        
+    }
+    
 }
+
+
